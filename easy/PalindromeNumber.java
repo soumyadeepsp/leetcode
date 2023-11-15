9c5cadda9c741b163d8a2f73fe13025cbfa5d5e5
soupaul_expedia <soupaul@expediagroup.com>
2023-11-15 18:22:23 +0530

    added PalindromeNumber.java

---
+++ easy/PalindromeNumber.java
@@ -0,0 +1,28 @@
+package easy;
+
+import java.util.Scanner;
+
+class PalindromeNumber {
+    public static boolean isPalindrome(int n) {
+        if (n<0) {
+            return false;
+        }
+        int revertedN = 0;
+        int x = n;
+        while (n>0) {
+            revertedN = revertedN*10 + n%10;
+            n = n/10;
+        }
+        System.out.println(x+" "+revertedN);
+        if (x==revertedN) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        System.out.println(isPalindrome(n));
+    }
+}
