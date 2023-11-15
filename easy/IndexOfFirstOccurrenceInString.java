0052fb0633466196ab68309d2768f22030caeb88
soupaul_expedia <soupaul@expediagroup.com>
2023-11-16 00:13:40 +0530

    added IndexOfFirstOccurrenceInString.java

---
+++ easy/IndexOfFirstOccurrenceInString.java
@@ -0,0 +1,29 @@
+package easy;
+
+import java.util.Scanner;
+
+public class IndexOfFirstOccurrenceInString {
+    public static int strStr(String haystack, String needle) {
+        for (int i=0; i<haystack.length(); i++) {
+            if (haystack.charAt(i)==needle.charAt(0)) {
+                int c = 0;
+                for (int j=0; j<needle.length(); j++) {
+                    if ((i+j<haystack.length()) && (haystack.charAt(i+j)==needle.charAt(j))) {
+                        ++c;
+                    }
+                }
+                if (c==needle.length()) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String haystack = scan.next();
+        String needle = scan.next();
+        System.out.println(strStr(haystack, needle));
+        scan.close();
+    }
+}
