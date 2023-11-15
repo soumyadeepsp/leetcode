dddf80712a2caed514bfb4cb13fccae30dce8e15
soupaul_expedia <soupaul@expediagroup.com>
2023-11-15 19:01:55 +0530

    added ValidParenthesis.java and LongestCommonPrefix.java

---
+++ easy/LongestCommonPrefix.java
@@ -0,0 +1,40 @@
+package easy;
+
+import java.util.Scanner;
+
+public class LongestCommonPrefix {
+    public static String longestCommonPrefix(String[] arr) {
+        int min_length = Integer.MAX_VALUE;
+        for (int i=0; i<arr.length; i++) {
+            if (arr[i].length()<min_length) {
+                min_length = arr[i].length();
+            }
+        }
+        String ans = "";
+        int c = 0;
+        for (int j=0; j<min_length; j++) {
+            char x = arr[0].charAt(j);
+            for (int i=0; i<arr.length; i++) {
+                if (arr[i].charAt(j)!=x) {
+                    ++c;
+                }
+            }
+            if (c==0) {
+                ans = ans + x;
+            } else {
+                break;
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        String[] arr = new String[n];
+        for (int i=0; i<n; i++) {
+            arr[i] = scan.next();
+        }
+        scan.close();
+        System.out.println(longestCommonPrefix(arr));
+    }
+}
---
+++ easy/ValidParenthesis.java
@@ -0,0 +1,37 @@
+package easy;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class ValidParenthesis {
+    public static boolean validParenthesis(String s) {
+        Stack<Character> st = new Stack<>();
+        for (int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+            if (c=='[' || c=='{' || c=='(') {
+                st.add(c);
+            } else {
+                if (c==']' && !st.isEmpty() && st.peek()=='[') {
+                    st.pop();
+                } else if (c=='}' && !st.isEmpty() && st.peek()=='{') {
+                    st.pop();
+                } else if (c==')' && !st.isEmpty() && st.peek()=='(') {
+                    st.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        if (st.isEmpty()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        System.out.println(validParenthesis(s));
+        scan.close();
+    }
+}
