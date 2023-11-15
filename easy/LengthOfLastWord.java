aa9ee46745d515d6ea1cc797ecaa1ad4481af268
soupaul_expedia <soupaul@expediagroup.com>
2023-11-16 00:27:06 +0530

    added LengthOfLastWord.java

---
+++ easy/LengthOfLastWord.java
@@ -0,0 +1,24 @@
+package easy;
+
+import java.util.Scanner;
+
+public class LengthOfLastWord {
+    public static int lengthOfLastWord(String s) {
+        int c = 0;
+        for (int i=s.length()-1; i>=0; i--) {
+            if (s.charAt(i)!=' ') {
+                ++c;
+            }
+            if (i>0 && s.charAt(i)!=' ' && s.charAt(i-1)==' ') {
+                break;
+            }
+        }
+        return c;
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        System.out.println(lengthOfLastWord(s));
+        scan.close();
+    }
+}
