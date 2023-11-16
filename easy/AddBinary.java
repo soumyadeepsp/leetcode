17499500a75dc6d0ccf8289372dc8dc7c6927c90
soupaul_expedia <soupaul@expediagroup.com>
2023-11-16 15:27:48 +0530

    added AddBinary.java

---
+++ easy/AddBinary.java
@@ -0,0 +1,45 @@
+package easy;
+
+import java.util.Scanner;
+
+public class AddBinary {
+    public static String addBinary(String a, String b) {
+        String ans = "";
+        int i = a.length()-1;
+        int j = b.length()-1;
+        int carry = 0;
+        while (i>=0 && j>=0) {
+            char x1 = a.charAt(i);
+            char x2 = b.charAt(j);
+            int x = Integer.parseInt(x1+"") + Integer.parseInt(x2+"") + carry;
+            carry = x/2;
+            ans = x%2 + ans;
+            i--; j--;
+        }
+        while (i>=0) {
+            char x1 = a.charAt(i);
+            int x = Integer.parseInt(x1+"") + carry;
+            carry = x/2;
+            ans = x%2 + ans;
+            i--;
+        }
+        while (j>=0) {
+            char x2 = b.charAt(j);
+            int x = Integer.parseInt(x2+"") + carry;
+            carry = x/2;
+            ans = x%2 + ans;
+            j--;
+        }
+        if (carry==1) {
+            ans = carry + ans;
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        System.out.println(addBinary(a, b));
+        scan.close();
+    }
+}
