07ff5011e462002cc410e4b03d561cd73003710f
soupaul_expedia <soupaul@expediagroup.com>
2023-11-18 00:55:23 +0530

    added PowerOfThree.java

---
+++ easy/PowerOfThree.java
@@ -0,0 +1,24 @@
+package easy;
+
+import java.util.Scanner;
+
+public class PowerOfThree {
+    public static boolean isPowerOfThree(int n) {
+        if (n<=0) {
+            return false;
+        }
+        if (n==1) {
+            return true;
+        }
+        if (n%3!=0) {
+            return false;
+        }
+        return isPowerOfThree(n/3);
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        System.out.println(isPowerOfThree(n));
+        scan.close();
+    }
+}
