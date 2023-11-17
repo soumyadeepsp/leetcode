efd3e97f15f79c95e4829d51e68658ab8a09e5a2
soupaul_expedia <soupaul@expediagroup.com>
2023-11-17 18:29:47 +0530

    added IsPalindrome.java

---
+++ easy/IsPalindrome.java
@@ -0,0 +1,56 @@
+package easy;
+
+import java.util.Scanner;
+
+public class IsPalindrome {
+    public static ListNode left = null;
+    public static void print(ListNode head) {
+        if (head==null) {
+            return;
+        }
+        System.out.print(head.val+" ");
+        print(head.next);
+    }
+    public static boolean isPalindrome(ListNode head) {
+        if (head==null) {
+            return true;
+        }
+        if (left==null) {
+            left = head;
+        }
+        boolean smallerProblem = isPalindrome(head.next);
+        ListNode right = head;
+        if (left.val!=right.val) {
+            return false;
+        }
+        left = left.next;
+        return smallerProblem;
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int data = scan.nextInt();
+        ListNode head = null;
+        ListNode temp = head;
+        while (data!=-1) {
+            ListNode newNode = new ListNode(data);
+            if (head==null) {
+                head = newNode;
+                temp = head;
+            } else {
+                temp.next = newNode;
+                temp = temp.next;
+            }
+            data = scan.nextInt();
+        }
+        System.out.println(isPalindrome(head));
+        scan.close();
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int val) {
+        this.val = val;
+    }
+}
