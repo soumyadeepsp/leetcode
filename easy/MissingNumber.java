f952eca0969804dfe72dc4780197e4e9e6327977
soupaul_expedia <soupaul@expediagroup.com>
2023-11-18 14:58:25 +0530

    added MissingNumber.java

---
+++ easy/MissingNumber.java
@@ -0,0 +1,28 @@
+package easy;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+// Given an array nums containing n distinct numbers in the range [0, n], 
+// return the only number in the range that is missing from the array.
+
+public class MissingNumber {
+    public static int missingNumber(int[] nums) {
+        HashMap<Integer, Boolean> map = new HashMap<>();
+        for (int i=0; i<nums.length; i++) {
+            map.put(nums[i], true);
+        }
+        for (int i=0; i<nums.length+1; i++) {
+            if (!map.containsKey(i)) {
+                return i;
+            }
+        }
+        return 0;
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        System.out.println(missingNumber(n));
+        scan.close();
+    }
+}
